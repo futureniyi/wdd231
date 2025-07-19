db07282d45a8b644f18bc8399aed2e6c34e11211
futureniyi <futureniyi@yahoo.com>
2025-07-20 00:11:30 +0100

    week03

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -10,66 +10,7 @@ async function getMemberData() {
 
 getMemberData();
 
-// const displayMembers = (members) => {
-//     members.forEach((member) => {
-//         // Create a section element for the card
-//         const card = document.createElement('section');
 
-//         // Create an h2 element for the full name
-//         const fullName = document.createElement('p');
-//         fullName.textContent = `${member.name}`;
-
-//         // Create a p element 
-//         const address = document.createElement('p');
-//         address.textContent = `${member.address}`;
-
-//         // Create a p element 
-//         const phoneNumber = document.createElement('p');
-//         phoneNumber.textContent = `${member.phoneNumber}`;
-
-//         // Create a p element 
-//         // const website = document.createElement('p');
-//         // website.textContent = `Website: ${member.website}`;
-//         const website = document.createElement('p');
-//         website.innerHTML = `<a href="${member.website}" target="_blank">${member.website}</a>`;
-
-
-//         // Create a p element 
-//         const membershipLevel = document.createElement('p');
-//         membershipLevel.textContent = `${member.membershipLevel}`;
-
-//         // const levelNames = { 1: 'Member', 2: 'Silver', 3: 'Gold' };
-//         // membershipLevel.textContent = `Membership Level: ${levelNames[member.membershipLevel] || 'Unknown'}`;
-
-
-//         // Create a p element 
-//         const founderName = document.createElement('p');
-//         founderName.textContent = `Founder: ${member.founderName}`;
-        
-
-//         // Create an img element for the portrait
-//         const logo = document.createElement('img');
-//         logo.setAttribute('src', member.image);
-//         logo.setAttribute('alt', `Logo of ${member.name}`);
-//         logo.setAttribute('loading', 'lazy');
-//         logo.setAttribute('width', '340');
-//         logo.setAttribute('height', '440');
-
-//         // Append the fullName and portrait to the card
-//         card.appendChild(logo);
-//         card.appendChild(fullName);
-        
-//         card.appendChild(address)
-//         card.appendChild(phoneNumber)
-//         card.appendChild(website)
-//         card.appendChild(membershipLevel)
-//         card.appendChild(founderName)
-        
-
-//         // Append the card to the cards container
-//         cards.appendChild(card);
-//     });
-// };
 
 const displayMembers = (members) => {
     members.forEach((member) => {
